3e786c082aeb76e0d96191543e341a83882e6982
Giancarlo Fontela da Luz <gian.f.luz@hotmail.com>
2021-04-17 23:52:00 -0300

    Added alasql engine

--- src/engines.js
+++ src/engines.js
@@ -1,15 +1,24 @@
 import jmespath from 'jmespath';
 import jsonpath from 'jsonpath';
+import alasql from 'alasql';
 
 export default [
     {
         label: 'JMES Path',
         name: 'jmespath',
+        placeholder: '[*].a',
         engine: jmespath.search.bind(jmespath)
     },
     {
         label: 'JSON Path',
         name: 'jsonpath',
+        placeholder: '$..a',
         engine: jsonpath.query.bind(jsonpath)
+    },
+    {
+        label: 'Ala SQL',
+        name: 'alasql',
+        placeholder: 'SELECT a, sum(b) FROM ?',
+        engine: (input, query) => alasql(query, [ input ])
     }
 ];
